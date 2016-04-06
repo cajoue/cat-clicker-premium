7a60ed69daea7cdd04fc3d77090430a598e1b712
cajoue <cajoue@users.noreply.github.com>
2016-04-06 12:50:54 +0200

    feat: starter files from cat-clicker-duel

--- README.md
+++ README.md
@@ -1,1 +1,31 @@
 # cat-clicker-premium
+
+Frontend Nanodegree project
+
+Javascript application for cat clicker game
+
+Based on [Cat Clicker Duel](https://github.com/cajoue/cat-clicker-duel) 
+
+** Requirements for Cat Clicker Premium **
+
+_Visuals_
+
+* The application should display
+    - a list of at least 5 cats, listed by name
+    - an area to display the selected cat
+* In the cat display area, the following should be displayed
+    - the cat's name
+    - a picture of the cat
+    - text showing the number of clicks
+
+The specifics of the layout do not matter, so style it however you'd like.
+
+_Interaction_
+
+* When a cat name is clicked in the list, the cat display area should update to show the data for the selected cat.
+* The number of clicks in the cat area should be unique to each cat, and should increment when the cat's picture is clicked.
+
+**To Do:**
+
+**Helpful Resources**
+
---
+++ js/main.js
@@ -0,0 +1,82 @@
+'use strict';
+
+// helper functions to display html
+
+var catUnit = '<div class="cat-unit"><figure id="%data%"></figure></div>';
+var catName = '<figcaption><h3>%data%</h3></figcaption>';
+var catImage = '<picture><img src="%data%" alt="picture of kitten"></picture>';
+var catSource = '<figcaption>Kitten thanks to <a href="%data%</a></figcaption>';
+
+// cat JSON
+
+var cats = {
+    cat: [
+        {
+            id: 'cat01',
+            name: 'Katie',
+            image: 'http://placekitten.com/300/200',
+            sourceURL: 'http://placekitten.com',
+            source: 'placekitten.com',
+            count: 0
+        },
+        {
+            id: 'cat02',
+            name: 'Charlotte',
+            image: 'http://placekitten.com/g/300/200',
+            sourceURL: 'http://placekitten.com',
+            source: 'placekitten.com',
+            count: 0
+        }
+    ]
+};
+
+cats.display = function(){
+  for (var kitty = 0, kittyCount = cats.cat.length; kitty < kittyCount; kitty++){
+    var formatCatID = catUnit.replace('%data%', cats.cat[kitty].id);
+    $('#cats').append(formatCatID);
+
+    var formatCatName = catName.replace('%data%', cats.cat[kitty].name);
+    $('figure:last').append(formatCatName);
+
+    var formatCatImage = catImage.replace('%data%', cats.cat[kitty].image);
+    $('figure:last').append(formatCatImage);
+    $('figure:last').append('<figcaption class="kitInfo"></figcaption>');
+
+    var formatCatSource  = catSource.replace('%data%', cats.cat[kitty].sourceURL + '">' + cats.cat[kitty].source);
+    $('figure:last').append(formatCatSource);
+  }
+};
+
+cats.click = function(){
+  //
+};
+
+// use $(document).ready() for jQuery code in external js file
+// $(function(){}) is shorthand for $(document).ready(function(){})
+
+$(document).ready(function() {
+
+  $('picture').click(function (event) {
+
+    // identify which cat was clicked
+    var catID = $(this).parent().attr("id");
+
+    // find index of this cat in cat JSON
+    for (var i = 0, numCats = cats.cat.length; i < numCats; i++) {
+      if (cats.cat[i].id == catID) {
+        var index = i;
+      };
+    };
+
+    // get the parent and target the required sibling
+    // var target = $(this).closest('figure').children('.kitInfo');
+    var target = $(this).parent().children('.kitInfo');
+
+    // update number of clicks for the particular cat.
+    target.text('I has been clicked ' + ++cats.cat[index].count + ' times');
+    // alternatively
+    // $(this).next().text('I has been clicked ' + ++cats.cat[index].count + ' times');
+  });
+});
+
+cats.display();
